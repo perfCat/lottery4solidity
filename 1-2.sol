251f5739e4bbe682a1579c85405a11e35a99cd1d
perfCat <kobori@kaetaru.com>
2019-06-07 11:02:32 +0900

    add sample code 1-2

---
+++ 1-2.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.0;
+
+contract Lottery {
+
+  /* ------ 宝くじ ------ */
+  address payable[] private lotteries;
+
+  /* ------ 宝くじの設定 ------ */
+  uint private winningAmount;       // 当選額
+  uint private maxSales = 10;       // 宝くじの販売数
+
+  /* コンストラクタ */
+  constructor(uint _winningAmount) public payable {
+    winningAmount = _winningAmount * 1e18;
+    require(msg.value >= winningAmount, "保有金額が不足しています。");
+  }
+
+  // 宝くじの当選金額を取得する
+  function checkWinningAmount() public view returns (uint) {
+      return winningAmount;
+  }
+
+  // 宝くじを購入
+  function buy() public {
+    lotteries.push(msg.sender);
+  }
+
+  // 抽選
+  function decide(uint decidedIndex) public {
+    address payable addr = lotteries[decidedIndex];
+    addr.transfer(winningAmount);
+  }
+}
+
